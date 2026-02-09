45677f776bc56664f489a680b35ca5c9c79e4741
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-02-09 16:19:52 -0300

    wip: implementado via IA parte de produção e orçamento

--- app/Filament/Clusters/Sales/Resources/Quotes/Schemas/QuoteForm.php
+++ app/Filament/Clusters/Sales/Resources/Quotes/Schemas/QuoteForm.php
@@ -172,7 +172,7 @@ class QuoteForm
                                     ->required(),
                                 Select::make('unit_of_measure')
                                     ->label('Unid.')
-                                    ->columnSpan(['md' => 1, 'lg' => 1])
+                                    ->columnSpan(['md' => 2, 'lg' => 2])
                                     ->options(Unit::toSelectArray())
                                     ->native(false)
                                     ->default('UN')
