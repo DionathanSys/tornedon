8ea8bbf4fe3ae1f27418ce4df0ad53dc82d3158d
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-02-21 11:09:18 -0300

    fix: Ajustes

--- app/Filament/Clusters/Sales/Resources/ServiceOrders/RelationManagers/Actions/EditItemAction.php
+++ app/Filament/Clusters/Sales/Resources/ServiceOrders/RelationManagers/Actions/EditItemAction.php
@@ -77,8 +77,7 @@ final class EditItemAction
                     ->schema([
                         Money::make('discount_percentage')
                             ->label('Desconto (%)')
-                            ->suffix('%')
-                            ->prefix(null)
+                            
                             ->live(onBlur: true)
                             ->afterStateUpdated(function ($state, Set $set, callable $get) {
                                 $subtotal = self::parseMoneyValue($get('subtotal'));
