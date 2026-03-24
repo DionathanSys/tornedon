c97a8929b7615dcc6e234a5451dfdc76b9668df2
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-03-23 22:27:50 -0300

    fix

--- app/Filament/Clusters/Financial/Resources/FiscalDocuments/Schemas/FiscalDocumentForm.php
+++ app/Filament/Clusters/Financial/Resources/FiscalDocuments/Schemas/FiscalDocumentForm.php
@@ -7,11 +7,13 @@ use App\Enum\FiscalDocument\IssuePurpose;
 use App\Enum\FiscalDocument\OperationNature;
 use App\Enum\FiscalDocument\OperationType;
 use App\Enum\FiscalDocument\Status;
+use App\Filament\Clusters\Sales\Resources\Components\SelectPartner;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
@@ -22,53 +24,35 @@ class FiscalDocumentForm
         return $schema
             ->columns([
                 'sm' => 1,
-                'md' => 4,
+                'md' => 6,
                 'lg' => 12,
             ])
             ->components([
                 Section::make('Dados da Nota de Entrada')
                     ->columns([
                         'sm' => 1,
-                        'md' => 4,
+                        'md' => 6,
                         'lg' => 12,
                     ])
                     ->columnSpanFull()
                     ->schema([
-                        Select::make('customer_id')
-                            ->label('Fornecedor')
-                            ->columnSpan(['md' => 2, 'lg' => 4])
-                            ->relationship('customer', 'name')
-                            ->searchable()
-                            ->preload()
-                            ->required(),
-                        Select::make('invoice_id')
-                            ->label('Fatura')
-                            ->columnSpan(['md' => 2, 'lg' => 3])
-                            ->relationship('invoice', 'invoice_number')
-                            ->searchable()
-                            ->preload()
-                            ->nullable(),
+                        SelectPartner::make('customer_id')
+                            ->label('Fornecedor'),
                         Select::make('document_type')
                             ->label('Tipo Documento')
                             ->columnSpan(['md' => 1, 'lg' => 2])
                             ->options(DocumentModel::toSelectArray())
+                            ->default(DocumentModel::NFE->value)
                             ->native(false)
                             ->required(),
-                        Select::make('status')
+                        TextEntry::make('status')
                             ->label('Status')
                             ->columnSpan(['md' => 1, 'lg' => 3])
-                            ->options(Status::toSelectArray())
-                            ->native(false)
-                            ->default(Status::PENDING->value)
                             ->visibleOn('edit')
-                            ->disabled(),
+                            ->badge(),
                     ]),
                 Section::make('Identificação')
-                    ->columns([
-                        'sm' => 1,
-                        'md' => 4,
-                        'lg' => 12,
-                    ])
+                    ->columns(['sm' => 1,'md' => 6,'lg' => 12,])
                     ->columnSpanFull()
                     ->schema([
                         TextInput::make('document_number')
@@ -96,61 +80,32 @@ class FiscalDocumentForm
                             ->label('Natureza da Operação')
                             ->columnSpan(['md' => 2, 'lg' => 5])
                             ->options(OperationNature::toSelectArray())
+                            ->default(OperationNature::VENDA_DENTRO_ESTADO->value)
                             ->searchable()
                             ->required(),
                         Select::make('issue_purpose')
                             ->label('Finalidade de Emissão')
                             ->columnSpan(['md' => 1, 'lg' => 4])
                             ->options(IssuePurpose::toSelectArray())
+                            ->default(IssuePurpose::NORMAL->value)
                             ->native(false)
                             ->required(),
-                        DatePicker::make('issued_at')
-                            ->label('Data de Emissão')
-                            ->columnSpan(['md' => 1, 'lg' => 3])
-                            ->displayFormat('d/m/Y'),
                     ]),
                 Section::make('Datas')
-                    ->columns([
-                        'sm' => 1,
-                        'md' => 4,
-                        'lg' => 12,
-                    ])
+                    ->columns(['sm' => 1,'md' => 6,'lg' => 12,])
                     ->columnSpanFull()
                     ->schema([
+                        DatePicker::make('issued_at')
+                            ->label('Data de Emissão')
+                            ->columnSpan(['md' => 2, 'lg' => 3])
+                            ->displayFormat('d/m/Y')
+                            ->default(now()),
                         DatePicker::make('movement_at')
                             ->label('Data de Entrada')
-                            ->columnSpan(['md' => 2, 'lg' => 4])
-                            ->displayFormat('d/m/Y'),
+                            ->columnSpan(['md' => 2, 'lg' => 3])
+                            ->displayFormat('d/m/Y')
+                            ->default(now()),
                     ]),
-                Section::make('Observações')
-                    ->columns([
-                        'sm' => 1,
-                        'md' => 4,
-                        'lg' => 12,
-                    ])
-                    ->columnSpanFull()
-                    ->collapsible()
-                    ->collapsed()
-                    ->persistCollapsed()
-                    ->schema([
-                        Textarea::make('tax_observations')
-                            ->label('Observações Fiscais')
-                            ->columnSpan(['md' => 2, 'lg' => 6])
-                            ->rows(3),
-                        Textarea::make('additional_tax_information')
-                            ->label('Informações Fiscais Adicionais')
-                            ->columnSpan(['md' => 2, 'lg' => 6])
-                            ->rows(3),
-                        Textarea::make('taxpayer_observations')
-                            ->label('Observações do Contribuinte')
-                            ->columnSpan(['md' => 2, 'lg' => 6])
-                            ->rows(3),
-                        Textarea::make('additional_purchase_information')
-                            ->label('Informações Adicionais de Compra')
-                            ->columnSpanFull()
-                            ->rows(3),
-                    ]),
-                // Campos fixos — não exibidos ao usuário
                 Hidden::make('company_id'),
                 Hidden::make('operation_type')->default(OperationType::ENTRADA->value),
             ]);
