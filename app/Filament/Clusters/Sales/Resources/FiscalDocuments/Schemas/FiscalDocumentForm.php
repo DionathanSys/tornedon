da029b887e5188ab59d4a305957456e5ea92a0de
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-03-25 23:11:21 -0300

    fix

--- app/Filament/Clusters/Sales/Resources/FiscalDocuments/Schemas/FiscalDocumentForm.php
+++ app/Filament/Clusters/Sales/Resources/FiscalDocuments/Schemas/FiscalDocumentForm.php
@@ -95,7 +95,7 @@ class FiscalDocumentForm
                                             ->visibleOn('edit')
                                             ->visible(fn($record): bool => $record->isNfse())
                                             ->columnSpan(['md' => 1, 'lg' => 2])
-                                            ->formatStateUsing(fn(NfeStatus $state): ?string => $state->description() ?? 'N/D')
+                                            ->formatStateUsing(fn(NfeStatus $state): ?string => dd($state->description()) ?? 'N/D')
                                             ->badge()
                                             ->color(fn(NfeStatus $state) => $state->color()),
                                         TextEntry::make('nfe_status')
