c9b62a822f7115b0e0fd1fc8f8ca14e0e26452eb
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-03-30 18:19:12 -0300

    fix

--- app/Filament/Mobile/Resources/Equipments/Pages/EditEquipment.php
+++ app/Filament/Mobile/Resources/Equipments/Pages/EditEquipment.php
@@ -3,10 +3,13 @@
 namespace App\Filament\Mobile\Resources\Equipments\Pages;
 
 use App\Filament\Mobile\Resources\Equipments\EquipmentResource;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Enums\Size;
 
 class EditEquipment extends EditRecord
 {
@@ -15,8 +18,17 @@ class EditEquipment extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
-            RestoreAction::make(),
+            ActionGroup::make([
+                Action::make('index')
+                    ->label('Voltar')
+                    ->icon('heroicon-o-arrow-left')
+                    ->url(EquipmentResource::getUrl())
+                    ->size(Size::ExtraSmall),
+                DeleteAction::make()
+                    ->size(Size::ExtraSmall),
+                RestoreAction::make()
+                    ->size(Size::ExtraSmall),
+            ])->buttonGroup(),
         ];
     }
 }
