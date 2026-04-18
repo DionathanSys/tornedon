d2cb74c773292592cc9f8dbf6c68f29a46080432
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-04-18 08:22:48 -0300

    fix

--- app/Filament/Clusters/Financial/Resources/AccountReceivables/AccountReceivableResource.php
+++ app/Filament/Clusters/Financial/Resources/AccountReceivables/AccountReceivableResource.php
@@ -29,7 +29,7 @@ class AccountReceivableResource extends Resource
 
     protected static ?string $pluralModelLabel = 'Contas à Receber';
 
-    protected static ?int $navigationSort = 9;
+    protected static ?int $navigationSort = 8;
 
     public static function form(Schema $schema): Schema
     {
