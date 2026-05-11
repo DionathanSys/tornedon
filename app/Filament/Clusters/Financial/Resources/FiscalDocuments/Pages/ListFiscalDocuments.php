e9ebcf6cc89844b62508e083b75dc199dd0bea08
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2026-05-10 23:59:21 -0300

    fix

--- app/Filament/Clusters/Financial/Resources/FiscalDocuments/Pages/ListFiscalDocuments.php
+++ app/Filament/Clusters/Financial/Resources/FiscalDocuments/Pages/ListFiscalDocuments.php
@@ -4,13 +4,24 @@ namespace App\Filament\Clusters\Financial\Resources\FiscalDocuments\Pages;
 
 use App\Filament\Clusters\Financial\Resources\FiscalDocuments\Actions\ImportSefazDfeAction;
 use App\Filament\Clusters\Financial\Resources\FiscalDocuments\FiscalDocumentResource;
+use App\Filament\Clusters\Financial\Resources\FiscalDocuments\Widgets\FiscalDocumentsStatsOverview;
 use Filament\Actions\CreateAction;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Icons\Heroicon;
 
 class ListFiscalDocuments extends ListRecords
 {
+    use ExposesTableToWidgets;
+
     protected static string $resource = FiscalDocumentResource::class;
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            FiscalDocumentsStatsOverview::class,
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
---
+++ app/Filament/Clusters/Financial/Resources/FiscalDocuments/Widgets/FiscalDocumentsStatsOverview.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Clusters\Financial\Resources\FiscalDocuments\Widgets;
+
+use App\Filament\Clusters\Financial\Resources\FiscalDocuments\Pages\ListFiscalDocuments;
+use App\Models\FiscalDocumentItem;
+use Filament\Widgets\Concerns\InteractsWithPageTable;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class FiscalDocumentsStatsOverview extends StatsOverviewWidget
+{
+    use InteractsWithPageTable;
+
+    protected function getTablePage(): string
+    {
+        return ListFiscalDocuments::class;
+    }
+
+    protected function getStats(): array
+    {
+        $documentIdsQuery = $this->getPageTableQuery()
+            ->clone()
+            ->reorder()
+            ->select('fiscal_documents.id');
+
+        $itemsTotal = FiscalDocumentItem::query()
+            ->whereIn('fiscal_document_id', $documentIdsQuery)
+            ->sum('total_price');
+
+        return [
+            Stat::make('Total dos documentos', $this->formatMoney($itemsTotal / 100))
+                ->description('Valor total dos documentos'),
+        ];
+    }
+
+    private function formatMoney(float $amount): string
+    {
+        return 'R$ ' . number_format($amount, 2, ',', '.');
+    }
+}
