4f34a981a825b48fd1a4711a2b33837ea93b5f03
DionathanSys <dionideev@gmail.com>
2026-06-20 06:24:46 -0300

    fix

--- app/Filament/Clusters/Financial/Resources/FiscalDocuments/Actions/CreateServiceOrderFromEntryAction.php
+++ app/Filament/Clusters/Financial/Resources/FiscalDocuments/Actions/CreateServiceOrderFromEntryAction.php
@@ -36,7 +36,7 @@ final class CreateServiceOrderFromEntryAction
             ->color('primary')
             ->modalWidth(Width::ExtraLarge)
             ->modalHeading('Criar ordem de serviço a partir da nota')
-            ->visible(fn (FiscalDocument $record): bool => dd($record->operation_type->value, OperationType::ENTRADA->value, $record->operation_type === OperationType::ENTRADA->value))
+            ->visible(fn (FiscalDocument $record): bool => $record->operation_type == OperationType::ENTRADA)
             ->schema([
                 Tabs::make('createServiceOrderFromEntryTabs')
                     ->columnSpanFull()
